3ba0ee0393d9ae1cc2c6a2e527b8f52c6c5bf65b
Salil Gupta <g.salil@live.in>
2022-10-10 17:20:30 +0530

    Some minor fix and redundant code removal

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const Spinner = () => {
   return (
-    <div className="text-center w-100vw">
-      <div className="spinner-border" role="status" />
+    <div className='text-center w-100vw'>
+      <div className='spinner-border' role='status' />
     </div>
   );
 };
--- src/Components/TweetContainer.js
+++ src/Components/TweetContainer.js
@@ -1,61 +1,56 @@
-import React, { useState, useEffect } from "react";
-import Tweet from "./Tweet";
-import Spinner from "./Spinner";
-import InfiniteScroll from "react-infinite-scroll-component";
-import logo from "../logo.png";
+import React, { useState, useEffect } from 'react';
+import Tweet from './Tweet';
+import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import logo from '../logo.png';
 
 export default function TweetContainer(props) {
   let { form } = props;
   const [tweet, setTweet] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
 
   const loadTweets = async () => {
-    const headers = { "Content-Type": "application/json" };
+    const headers = { 'Content-Type': 'application/json' };
     let url = `https://remotebirdjobs-api.herokuapp.com/search/?search=${form.topic}&start_date=${form.startDate}&end_date=${form.endDate}&page=${page}&wfh=${form.type}&pagesize=12`;
     fetch(url, { headers })
       .then((response) => response.json())
       .then((data) => setTweet(data))
       .catch((err) => {
-        console.log("Error => ", err);
-        setLoading(false);
+        console.log('Error => ', err);
       });
   };
   useEffect(() => {
-    setLoading(true);
     loadTweets();
     // eslint-disable-next-line
   }, [form]);
 
   const fetchMoreData = async () => {
     setPage(page + 1);
-    setLoading(true);
-    const headers = { "Content-Type": "application/json" };
+    const headers = { 'Content-Type': 'application/json' };
     let url = `https://remotebirdjobs-api.herokuapp.com/search/?search=${
       form.topic
     }&start_date=${form.startDate}&end_date=${form.endDate}&page=${
       page + 1
-    }&wfh=${form.type === "wfh" ? "true" : "false"}&pagesize=12`;
+    }&wfh=${form.type === 'wfh' ? 'true' : 'false'}&pagesize=12`;
     fetch(url, { headers })
       .then((response) => response.json())
       .then((data) => setTweet(tweet.concat(data)))
       .catch((err) => {
-        console.log("Error => ", err);
-        setLoading(false);
+        console.log('Error => ', err);
       });
   };
 
   return (
-    <div>
-      <div className="text-center">
-        <div className="row d-flex justify-content-center mx-0">
+    <>
+      <div className='text-center'>
+        <div className='row d-flex justify-content-center mx-0'>
           <img
-            className="col-auto my-3 mx-0"
-            style={{ borderRadius: "50%", width: "5%" }}
+            className='col-auto my-3 mx-0'
+            style={{ borderRadius: '50%', width: '5%' }}
             src={logo}
-            alt="logo"
+            alt='logo'
           />
-          <h1 className="col-auto my-3 mx-0 px-0">
+          <h1 className='col-auto my-3 mx-0 px-0'>
             <u>Tweets</u>
           </h1>
         </div>
@@ -65,27 +60,27 @@ export default function TweetContainer(props) {
         next={fetchMoreData}
         hasMore={tweet.length <= 47}
         endMessage={
-          <h3 className="text-center">Yay!!! You have seen it all</h3>
+          <h3 className='text-center'>Yay!!! You have seen it all</h3>
         }
         loader={<Spinner />}
       >
         <div
-          id="container"
-          className="d-flex flex-wrap align-items-center justify-content-space-between px-5 col"
+          id='container'
+          className='d-flex flex-wrap align-items-center justify-content-space-between px-5 col'
         >
           {tweet.map((element) => {
             return (
               <div
                 key={element.id}
-                className="m-3 mx-auto p-0 overflow-auto scrollbar"
-                style={{ width: "22%", height: "25vh", borderRadius: "12px" }}
+                className='m-3 mx-auto p-0 overflow-auto scrollbar'
+                style={{ width: '22%', height: '25vh', borderRadius: '12px' }}
               >
-                <Tweet id={element.id} className="p-0"></Tweet>
+                <Tweet id={element.id} className='p-0'></Tweet>
               </div>
             );
           })}
         </div>
       </InfiniteScroll>
-    </div>
+    </>
   );
 }
