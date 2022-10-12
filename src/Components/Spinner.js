ec59491d8a7e4d489048d1a0af379e4c3852049b
ozair-dev <truemail785@gmail.com>
2022-10-12 21:13:58 +0500

    #28-handle-empty-response and some code refactoring

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 import loading from "./loading.gif";
 
-const Spinner=()=>{
-    return (
-        <div className='text-center w-100vw'>
-            <img src={loading} className="my-3" alt="Loading" />
-        </div>
-    )
-    
-}
+const Spinner = () => {
+  return (
+    <div className="text-center my-3 w-100">
+      <img src={loading} className="d-block mx-auto" alt="Loading" />
+    </div>
+  );
+};
 
-export default Spinner
+export default Spinner;
